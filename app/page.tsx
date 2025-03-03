d229099718b04d2b733b609f90503875eb63fb88
NaN <nikita.wagner97@gmail.com>
2025-03-04 00:04:35 +0100

    module preparation test

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Header from './components/Header';
 import Hero from './components/Hero';
-import About from './components/About';
+import About from './components/About/About';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
